5b0e290ca670ec72dd309bb8aef4068e253a479d
fedebanca <fedebanca97@gmail.com>
2023-10-23 10:46:06 -0300

    Tiempo de reintento seteado a 1 segundo

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     },
     video: true,
     experimentalStudio: true,
-    scrollBehavior: "center"
+    scrollBehavior: "center",
+    defaultCommandTimeout: 1000
   },
 });
